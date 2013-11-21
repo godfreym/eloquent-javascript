147ddb5f9de75b89a4f3ba3c18178fae134654e8
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-11-21 14:14:28 +0000

    adding a function that finds a sequence

--- js/ch3.js
+++ js/ch3.js
@@ -4,3 +4,16 @@ return -number;
   else 
   return number;
 }
+
+function findSequence(goal) {
+  function find(start, history) {
+    if (start == goal)
+      return history;
+    else if (start > goal)
+      return null;
+    else
+      return find(start + 5, "(" + history + " + 5)") ||
+             find(start * 3, "(" + history + " * 3)");
+  }
+  return find(1, "1");
+}
