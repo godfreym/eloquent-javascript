ea599b5506822c3158e7c05df2a7811dc8303950
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-11-21 10:43:26 +0000

    creating example 2 that prints a triangle

--- js/ch2.js
+++ js/ch2.js
@@ -1,9 +1,19 @@
 ((4>=6) || ("grass" != "green")) &&
 !((12*2)==144 && true);
 
+/* Ex 2.2 */
 var result = 1;
 var counter = 0;
 while (counter < 10) {
   result = result * 2;
+  counter = counter + 1;
+}
+
+/* Ex 2.3 */
+var line = "";
+var counter = 0;
+while (counter < 10) {
+  line = line + "#";
+  document.write(line);
   counter = counter + 1;
 }
