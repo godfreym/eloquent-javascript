7669390d43f4aa7047299b02e27bb370e07381f4
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-11-21 11:29:40 +0000

    creating Ex 2.6 to modify Ex 2.5

--- js/ch2.js
+++ js/ch2.js
@@ -37,3 +37,19 @@ else if (answer == "3" || answer == "5")
   alert("Almost!");
 else
   alert("You're an embarrassment.");
+ 
+/* Ex 2.6 */ 
+var answer;
+while (true) {
+  answer = prompt("You! What is the value of 2 + 2?", "");
+  if (answer == "4") {
+    alert("You must be a genius or something.");
+    break;
+  }
+  else if (answer == "3" || answer == "5") {
+    alert("Almost!");
+  }
+  else {
+    alert("You're an embarrassment.");
+  }
+}
