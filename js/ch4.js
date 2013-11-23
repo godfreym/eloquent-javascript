afcf6660593e679bde2fb51cda72b0ec02a807d2
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-11-23 23:21:20 +0000

    calling a function using a firebug console

--- js/ch4.js
+++ js/ch4.js
@@ -25,3 +25,5 @@ function catNames(paragraph) {
   var colon = paragraph.indexOf(":");
   return paragraph.slice(colon + 2).split(", ");
 }
+console.log(catNames("born 20/09/2004 (mother Yellow Bess): " +
+              "Doctor Hobbles the 2nd, Noog"));
