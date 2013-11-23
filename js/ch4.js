9f359a0f4ee635690646176113357f929247c3e9
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-11-23 23:29:58 +0000

    removing console.log calling of a function

--- js/ch4.js
+++ js/ch4.js
@@ -25,5 +25,3 @@ function catNames(paragraph) {
   var colon = paragraph.indexOf(":");
   return paragraph.slice(colon + 2).split(", ");
 }
-console.log(catNames("born 20/09/2004 (mother Yellow Bess): " +
-              "Doctor Hobbles the 2nd, Noog"));
