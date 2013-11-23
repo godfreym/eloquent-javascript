6061ff67e72fc97672b17ff6a03ae7c098e7b51c
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-11-23 09:01:38 +0000

    Addind example 4 to calculate range of numbers

--- js/ch4.js
+++ js/ch4.js
@@ -1,3 +1,4 @@
+/* Ex 4.1 */
 var set = {"Spot": true};
 // Add "White Fang" to the set
 set["White Fang"] = true;
@@ -5,3 +6,11 @@ set["White Fang"] = true;
 delete set["Spot"];
 // See if "Asoka" is in the set
 console.log("Asoka" in set);
+
+/* Ex 4.2 */
+function range(upto) {
+  var result = [];
+  for (var i = 0; i <= upto; i++)
+    result[i] = i;
+  return result;
+}
