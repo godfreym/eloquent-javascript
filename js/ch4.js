29f4c6025260844262b357879545c688965b1737
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-11-24 00:36:35 +0000

    creating a function to find sum of numbers

--- js/ch4.js
+++ js/ch4.js
@@ -82,3 +82,12 @@ function range(start, end) {
     result.push(i);
   return result;
 }
+
+/* Ex 4.11 */
+function sum(numbers) {
+  var total = 0;
+  for (var i = 0; i < numbers.length; i++)
+    total += numbers[i];
+  return total;
+}
+console.log((sum(range(1, 10))));
