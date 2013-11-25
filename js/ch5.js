aa8bd4550115f55190ba976e02eca35d34fc2f94
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-11-25 11:35:55 +0000

    creating a function that pop out the last element from an array

--- js/ch5.js
+++ js/ch5.js
@@ -10,3 +10,22 @@ function processThing(thing) {
     currentThing = null;
   }
 }
+
+/* Another example */
+function lastElement(array) {
+  if (array.length > 0)
+    return array[array.length - 1];
+  else
+    throw "Can not take the last element of an empty array.";
+}
+
+function lastElementPlusTen(array) {
+  return lastElement(array) + 10;
+}
+
+try {
+  print(lastElementPlusTen([]));
+}
+catch (error) {
+  print("Something went wrong: ", error);
+}
