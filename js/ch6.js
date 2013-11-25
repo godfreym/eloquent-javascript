89f89f039ca34025fc3e917f24aecb1a7636695d
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-11-25 12:41:23 +0000

    more modification of examples 6.2 and 6.3

--- js/ch6.js
+++ js/ch6.js
@@ -72,3 +72,37 @@ function splitParagraph(text) {
   }
   return fragments;
 }
+
+function processParagraph(paragraph) {
+  var header = 0;
+  while (paragraph.charAt(0) == "%") {
+    paragraph = paragraph.slice(1);
+    header++;
+  }
+
+  return {type: (header == 0 ? "p" : "h" + header),
+          content: splitParagraph(paragraph)};
+}
+function extractFootnotes(paragraphs) {
+  var footnotes = [];
+  var currentNote = 0;
+
+  function replaceFootnote(fragment) {
+    if (fragment.type == "footnote") {
+      currentNote++;
+      footnotes.push(fragment);
+      fragment.number = currentNote;
+      return {type: "reference", number: currentNote};
+    }
+    else {
+      return fragment;
+    }
+  }
+
+  forEach(paragraphs, function(paragraph) {
+    paragraph.content = map(replaceFootnote,
+                            paragraph.content);
+  });
+
+  return footnotes;
+}     
