9dfadfd33fb39e848757fc2db9a7c90e65baefb0
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-11-25 12:03:10 +0000

    modification of function countZeroes to behave a more functional programe

--- js/ch6.js
+++ js/ch6.js
@@ -5,3 +5,18 @@ function countZeroes(array) {
   }
   return reduce(counter, 0, array);
 }
+
+/* Ex. 6.1 modification*/
+function count(test, array) {
+  return reduce(function(total, element) {
+    return total + (test(element) ? 1 : 0);
+  }, 0, array);
+}
+
+function equals(x) {
+  return function(element) {return x === element;};
+}
+
+function countZeroes(array) {
+  return count(equals(0), array);
+}
