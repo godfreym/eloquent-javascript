c5768e9eac31edc6cb03ddb97fe1c79e24952a71
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-11-25 12:31:19 +0000

    declaring function processParagraph to process the paragraphs

--- js/ch6.js
+++ js/ch6.js
@@ -20,3 +20,15 @@ function equals(x) {
 function countZeroes(array) {
   return count(equals(0), array);
 }
+
+/* Ex. 6.2 */
+function processParagraph(paragraph) {
+  var header = 0;
+  while (paragraph.charAt(0) == "%") {
+    paragraph = paragraph.slice(1);
+    header++;
+  }
+
+  return {type: (header == 0 ? "p" : "h" + header),
+          content: paragraph};
+}
