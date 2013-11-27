fe334f0c5c3dfd94b13c57ff96f94484d94b50c9
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-11-27 11:02:33 +0000

    applying another words for a fat rabbit to say

--- js/ch8.js
+++ js/ch8.js
@@ -6,3 +6,4 @@ var fatRabbit = {adjective: "fat", speak: speak};
 
 whiteRabbit.speak("Oh my ears and whiskers, how late it's getting!");
 fatRabbit.speak("I could sure use a carrot right now.");
+speak.apply(fatRabbit, ["Yum."]);
