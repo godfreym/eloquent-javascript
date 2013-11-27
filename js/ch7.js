2a458ebd53da6cf064560139c80d4f37f2197b93
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-11-27 06:47:44 +0000

    creating another function filter to filter our data

--- js/ch7.js
+++ js/ch7.js
@@ -131,3 +131,16 @@ function flatten(arrays) {
     });
     return result;
   }
+  
+  
+/* Ex. 7.2 */
+function filter(test, array) {
+  var result = [];
+  forEach(array, function (element) {
+    if (test(element))
+      result.push(element);
+  });
+  return result;
+}
+
+console.log(filter(partial(op[">"], 5), [0, 4, 8, 12]));
