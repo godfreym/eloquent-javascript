94db76d2f8e2983770169d183e9360bd5db296ce
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-11-27 05:52:35 +0000

    adding a function makeRoad to create road with its starting point and destination

--- js/ch7.js
+++ js/ch7.js
@@ -1,3 +1,16 @@
+
+var roads = {};
+function makeRoad(from, to, length) {
+  function addRoad(from, to) {
+    if (!(from in roads))
+      roads[from] = [];
+    roads[from].push({to: to, distance: length});
+  }
+  addRoad(from, to);
+  addRoad(to, from);
+}
+
+/* Ex.7.1 */
 function makeRoads(start) {
   for (var i = 1; i < arguments.length; i += 2)
     makeRoad(start, arguments[i], arguments[i + 1]);
