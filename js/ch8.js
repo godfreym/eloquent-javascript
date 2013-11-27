028fca6b94ef03b68c1b543afc961bf7cc8dc5c4
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-11-27 14:23:11 +0000

    creating a drunkbug

--- js/ch8.js
+++ js/ch8.js
@@ -306,3 +306,29 @@ Terrarium.prototype.stop = function() {
     this.running = null;
   }
 };
+
+/* Ex. 8.6 */
+Dictionary.prototype.names = function() {
+  var names = [];
+  this.each(function(name, value) {names.push(name);});
+  return names;
+};
+
+show(directions.names());
+
+function randomElement(array) {
+  if (array.length == 0)
+    throw new Error("The array is empty.");
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+show(randomElement(["heads", "tails"]));
+
+function DrunkBug() {};
+DrunkBug.prototype.act = function(surroundings) {
+  return {type: "move",
+          direction: randomElement(directions.names())};
+};
+DrunkBug.prototype.character = "~";
+
+creatureTypes.register(DrunkBug
