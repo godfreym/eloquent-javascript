2a576de4b616df61b1e2a98554228ffd6d6f0e3f
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-11-27 12:06:09 +0000

    creating a function to add two points

--- js/ch8.js
+++ js/ch8.js
@@ -100,3 +100,18 @@ var thePlan =
    "# o  #         o       ### #",
    "#    #                     #",
    "############################"];
+   
+   
+/* Ex. 8.1 */
+function Point(x, y) {
+  this.x = x;
+  this.y = y;
+}
+Point.prototype.add = function(other) {
+  return new Point(this.x + other.x, this.y + other.y);
+};
+Point.prototype.isEqualTo = function(other) {
+  return this.x == other.x && this.y == other.y;
+};
+
+console.log((new Point(3, 1)).add(new Point(2, 4)));
