f73f73cc53afd009c968736c49c2a89af4a1d44e
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-11-27 12:09:15 +0000

    creating a function grid

--- js/ch8.js
+++ js/ch8.js
@@ -115,3 +115,25 @@ Point.prototype.isEqualTo = function(other) {
 };
 
 console.log((new Point(3, 1)).add(new Point(2, 4)));
+
+/* modification... */
+function Grid(width, height) {
+  this.width = width;
+  this.height = height;
+  this.cells = new Array(width * height);
+}
+Grid.prototype.valueAt = function(point) {
+  return this.cells[point.y * this.width + point.x];
+};
+Grid.prototype.setValueAt = function(point, value) {
+  this.cells[point.y * this.width + point.x] = value;
+};
+Grid.prototype.isInside = function(point) {
+  return point.x >= 0 && point.y >= 0 &&
+         point.x < this.width && point.y < this.height;
+};
+Grid.prototype.moveValue = function(from, to) {
+  this.setValueAt(to, this.valueAt(from));
+  this.setValueAt(from, undefined);
+};
+
