ff66319c1975eab86d05669109436b5553af6c98
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-11-27 13:55:05 +0000

    creating a terrarium object

--- js/ch8.js
+++ js/ch8.js
@@ -206,3 +206,17 @@ function characterFromElement(element) {
 }
 
 console.log(characterFromElement(wall));
+
+/* Ex. 8.3 */
+Terrarium.prototype.toString = function() {
+  var characters = [];
+  var endOfLine = this.grid.width - 1;
+  this.grid.each(function(point, value) {
+    characters.push(characterFromElement(value));
+    if (point.x == endOfLine)
+      characters.push("\n");
+  });
+  return characters.join("");
+};
+var terrarium = new Terrarium(thePlan);
+console.log(terrarium.toString());
