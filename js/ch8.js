e8e0f793718d01e46a5e11da2cb69675839c281e
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-11-28 12:47:44 +0000

    controlling lichen-eaters on how to eat

--- js/ch8.js
+++ js/ch8.js
@@ -484,3 +484,17 @@ var terrarium = new LifeLikeTerrarium(lichenPlan);
 terrarium.onStep = partial(inPlacePrinter(), terrarium);
 terrarium.start();
 terrarium.stop();
+
+LichenEater.prototype.act = function(surroundings) {
+  var emptySpace = findDirections(surroundings, " ");
+  var lichen = findDirections(surroundings, "*");
+
+  if (this.energy >= 30 && emptySpace.length > 0)
+    return {type: "reproduce", direction: randomElement(emptySpace)};
+  else if (lichen.length > 1)
+    return {type: "eat", direction: randomElement(lichen)};
+  else if (emptySpace.length > 0)
+    return {type: "move", direction: randomElement(emptySpace)};
+  else
+    return {type: "wait"};
+};
