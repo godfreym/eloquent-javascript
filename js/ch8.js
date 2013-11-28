76aa1cbd3ee05208470e9eb142fdf6eaeca8c1ab
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-11-28 12:42:12 +0000

    creating a modified linchen

--- js/ch8.js
+++ js/ch8.js
@@ -444,3 +444,25 @@ function findDirections(surroundings, wanted) {
   });
   return found;
 }
+
+/* Ex. 8.7 */
+function LichenEater() {
+  this.energy = 10;
+}
+LichenEater.prototype.act = function(surroundings) {
+  var emptySpace = findDirections(surroundings, " ");
+  var lichen = findDirections(surroundings, "*");
+
+  if (this.energy >= 30 && emptySpace.length > 0)
+    return {type: "reproduce", direction: randomElement(emptySpace)};
+  else if (lichen.length > 0)
+    return {type: "eat", direction: randomElement(lichen)};
+  else if (emptySpace.length > 0)
+    return {type: "move", direction: randomElement(emptySpace)};
+  else
+    return {type: "wait"};
+};
+LichenEater.prototype.character = "c";
+
+creatureTypes.register(LichenEater);
+
