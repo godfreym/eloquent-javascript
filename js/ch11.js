43e9db10fceb9af2b5dfacb52d3611ab2d7bfef2
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-12-03 09:41:23 +0000

    creating a new javascript file ch11.js to show chapter 11 examples

---
+++ js/ch11.js
@@ -0,0 +1,7 @@
+/* Ex. 11.1 */
+function validInfo(form) {
+  return form.elements.name.value != "" &&
+    /^.+@.+\.\w{2,3}$/.test(form.elements.email.value);
+}
+
+console.log(validInfo(document.forms.userinfo));
