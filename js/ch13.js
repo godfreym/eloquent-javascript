f89d0da690bc5d0214c93cb2e9fcf74c7629df48
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-12-03 12:12:14 +0000

    creating ch13.js file

---
+++ js/ch13.js
@@ -0,0 +1,9 @@
+function registerEventHandler(node, event, handler) {
+  if (typeof node.addEventListener == "function")
+    node.addEventListener(event, handler, false);
+  else
+    node.attachEvent("on" + event, handler);
+}
+
+registerEventHandler($("button"), "click",
+                     function(){print("Click (2)");});
