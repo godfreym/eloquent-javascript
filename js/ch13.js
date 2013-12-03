8ca6b224cd46bcade8ae11f3789e14a3e39b0a7d
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-12-03 12:51:55 +0000

    creating a function that moves a player

--- js/ch13.js
+++ js/ch13.js
@@ -142,3 +142,34 @@ Square.clearContent = function() {
   this.content = null;
   removeElement(this.tableCell.lastChild);
 };
+
+
+/* Ex. 13.3 */
+SokobanField.move = function(direction) {
+  var playerSquare = this.getSquare(this.playerPos);
+  var targetPos = this.playerPos.add(direction);
+  var targetSquare = this.getSquare(targetPos);
+
+  // Possibly pushing a boulder
+  if (targetSquare.hasBoulder()) {
+    var pushTarget = this.getSquare(targetPos.add(direction));
+    if (pushTarget.isEmpty()) {
+      targetSquare.moveContent(pushTarget);
+    }
+    else if (pushTarget.isExit()) {
+      targetSquare.moveContent(pushTarget);
+      pushTarget.clearContent();
+      this.bouldersToGo--;
+      this.updateScore();
+    }
+  }
+  // Moving the player
+  if (targetSquare.isEmpty()) {
+    playerSquare.moveContent(targetSquare);
+    this.playerPos = targetPos;
+  }
+};
+
+testField.move(new Point(-1, 0));
+testField.move(new Point(-1, 0));
+testField.remove();
