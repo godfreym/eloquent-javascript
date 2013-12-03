5ac0212167002f1032ed18ce3925a3724ea9d404
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-12-03 08:55:57 +0000

    creating example 10.1 to show how regular expression works

--- js/ch10.js
+++ js/ch10.js
@@ -2,3 +2,7 @@ var asteriskOrBrace = /[\{\*]/;
 var story =
   "We noticed the *giant sloth*, hanging from a giant branch.";
 console.log(story.search(asteriskOrBrace));
+
+/* Ex. 10.1 */
+var datePattern = /\d\d\/\d\d\/\d\d\d\d/;
+console.log("born 15/11/2003 (mother Spot): White Fang".search(datePattern));
