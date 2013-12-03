47201749760e59e5ced1386d30b2823a40b91cb6
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-12-03 09:04:45 +0000

    creating a function to validate date and format it in another format

--- js/ch10.js
+++ js/ch10.js
@@ -13,3 +13,14 @@ var mailAddress = /\b[\w\.-]+@[\w\.-]+\.\w{2,3}\b/;
 console.log(mailAddress.test("kenny@test.net"));
 console.log(mailAddress.test("I mailt kenny@tets.nets, but it didn wrok!"));
 console.log(mailAddress.test("the_giant_sloth@gmail.com"));
+
+/* Ex. 10.3 */
+function extractDate(string) {
+  var found = string.match(/(\d\d?)\/(\d\d?)\/(\d{4})/);
+  if (found == null)
+    throw new Error("No date found in '" + string + "'.");
+  return new Date(Number(found[3]), Number(found[2]) - 1,
+                  Number(found[1]));
+}
+
+console.log(extractDate("born 5/2/2007 (mother Noog): Long-ear Johnson"));
