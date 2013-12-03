828411e1a4a0e933c57ed2914fc4ba8579dd42b1
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-12-03 11:09:34 +0000

    creating a function to format contents as HTML

--- js/ch12.js
+++ js/ch12.js
@@ -11,3 +11,17 @@ function isImage(node) {
 }
 
 console.log(isImage(document.body.lastChild));
+
+/* Ex. 12.1 */
+function asHTML(node) {
+  if (isTextNode(node))
+    return escapeHTML(node.nodeValue);
+  else if (node.childNodes.length == 0)
+    return "<" + node.nodeName + "/>";
+  else
+    return "<" + node.nodeName + ">" +
+           map(asHTML, node.childNodes).join("") +
+           "</" + node.nodeName + ">";
+}
+
+console.log(asHTML(document.body));
