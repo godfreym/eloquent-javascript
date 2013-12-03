a88180834ecbcd7ddcc559233d20394041b29d41
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-12-03 09:00:31 +0000

    creating example 10.2 to validate email

--- js/ch10.js
+++ js/ch10.js
@@ -6,3 +6,10 @@ console.log(story.search(asteriskOrBrace));
 /* Ex. 10.1 */
 var datePattern = /\d\d\/\d\d\/\d\d\d\d/;
 console.log("born 15/11/2003 (mother Spot): White Fang".search(datePattern));
+
+/* Ex. 10.2 */
+var mailAddress = /\b[\w\.-]+@[\w\.-]+\.\w{2,3}\b/;
+
+console.log(mailAddress.test("kenny@test.net"));
+console.log(mailAddress.test("I mailt kenny@tets.nets, but it didn wrok!"));
+console.log(mailAddress.test("the_giant_sloth@gmail.com"));
