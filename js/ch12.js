8b289e7995299dfcf1057701c6d5cfb79a3087ee
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-12-03 11:21:33 +0000

    creating a function that makes a table

--- js/ch12.js
+++ js/ch12.js
@@ -72,3 +72,26 @@ function removeElement(node) {
 }
 
 removeElement(newParagraph)
+
+/* Ex. 12.3 */
+function makeTable(data, columns) {
+  var headRow = dom("TR");
+  forEach(columns, function(name) {
+    headRow.appendChild(dom("TH", null, name));
+  });
+
+  var body = dom("TBODY", null, headRow);
+  forEach(data, function(object) {
+    var row = dom("TR");
+    forEach(columns, function(name) {
+      row.appendChild(dom("TD", null, String(object[name])));
+    });
+    body.appendChild(row);
+  });
+
+  return dom("TABLE", null, body);
+}
+
+var table = makeTable(document.body.childNodes,
+                      ["nodeType", "tagName"]);
+document.body.appendChild(table);
