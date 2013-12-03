df3eada38a7fee8008a7cf99f55173c942cfef86
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-12-03 13:05:52 +0000

    creating an example 13.5 to clear content for square variable

--- js/ch13.js
+++ js/ch13.js
@@ -233,3 +233,22 @@ SokobanGame.keyDown = function(event) {
     }
   }
 };
+
+
+/* Ex. 13.5 */
+Square.clearContent = function() {
+  self.content = null;
+  var image = this.tableCell.lastChild;
+  var size = 100;
+
+  var animate = setInterval(function() {
+    size -= 10;
+    image.style.width = size + "%";
+    image.style.height = size + "%";
+
+    if (size < 60) {
+      clearInterval(animate);
+      removeElement(image);
+    }
+  }, 70);
+};
