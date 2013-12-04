6c8a546e89bb130703a85d8cd4396ddfc1f20c01
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-12-04 12:51:04 +0000

    creatinng a function with switch inside

--- js/appendix1.js
+++ js/appendix1.js
@@ -22,3 +22,22 @@ outer: for (var sideA = 1; sideA < 10; sideA++) {
     }
   }
 }
+
+/* switch and case */
+function weatherAdvice(weather) {
+  switch(weather) {
+    case "rainy":
+      console.log("Remember to bring an umbrella.");
+      break;
+    case "sunny":
+      console.log("Dress lightly.");
+    case "cloudy":
+      console.log("Go outside.");
+      break;
+    default:
+      console.log("Unknown weather type: ", weather);
+      break;
+  }
+}
+
+weatherAdvice("sunny");
