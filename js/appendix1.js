1e30be5dd428057b3d7409dda4b387bceb8c6177
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-12-04 12:38:55 +0000

    adding another loop to check number divisible by 3

--- js/appendix1.js
+++ js/appendix1.js
@@ -2,3 +2,10 @@ do {
   var answer = prompt("Say 'moo'.", "");
   console.log("You said '", answer, "'.");
 } while (answer != "moo");
+
+/* other loops */
+for (var i = 0; i < 10; i++) {
+  if (i % 3 != 0)
+    continue;
+  console.log(i, " is divisible by three.");
+}
