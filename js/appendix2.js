416e6f6f899aa714a0c4f7f3aaab874b13f13d81
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-12-04 13:17:19 +0000

    testing the heap using binaryHeap function

--- js/appendix2.js
+++ js/appendix2.js
@@ -110,3 +110,12 @@ BinaryHeap.prototype = {
     }
   }
 };
+
+/* Testing our heap */
+var heap = new BinaryHeap(function(x){return x;});
+forEach([10, 3, 4, 8, 2, 9, 7, 1, 2, 6, 5],
+        method(heap, "push"));
+
+heap.remove(2);
+while (heap.size() > 0)
+  console.log(heap.pop());
