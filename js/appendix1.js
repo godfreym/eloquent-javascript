502311c20b231ac93e826dbbaa5bf1c1f6ded432
Godfrey Mlowe <mlowegodfrey@gmail.com>
2013-12-04 12:44:31 +0000

    creating an inner and outer loop

--- js/appendix1.js
+++ js/appendix1.js
@@ -9,3 +9,16 @@ for (var i = 0; i < 10; i++) {
     continue;
   console.log(i, " is divisible by three.");
 }
+
+/* inner and outer loop */
+outer: for (var sideA = 1; sideA < 10; sideA++) {
+  inner: for (var sideB = 1; sideB < 10; sideB++) {
+    var hypotenuse = Math.sqrt(sideA * sideA + sideB * sideB);
+    if (hypotenuse % 1 == 0) {
+      console.log("A right triangle with straight sides of length ",
+            sideA, " and ", sideB, " has a hypotenuse of ",
+            hypotenuse, ".");
+      break outer;
+    }
+  }
+}
